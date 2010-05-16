637f5caf4724d4e4ba67067e54a1165315ad5c33
Steve Streza <stevestreza@gmail.com>
2010-05-16 00:33:29 -0400

    It works, holy crap

---
+++ Classes/MPPianobarPlayerController.h
@@ -0,0 +1,16 @@
+//
+//  MPPianobarPlayerController.h
+//  MiniPiano
+//
+//  Created by Steve Streza on 5/16/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface MPPianobarPlayerController : UIViewController {
+
+}
+
+@end
