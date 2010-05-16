33fb103965957a98e88c67f9c9f2fbffff5cf0f6
Steve Streza <stevestreza@gmail.com>
2010-05-16 12:10:52 -0400

    Readding clean app delegate class

---
+++ Classes/MiniPianoAppDelegate.m
@@ -0,0 +1,117 @@
+//
+//  MiniPianoAppDelegate.m
+//  MiniPiano
+//
+//  Created by Steve Streza on 5/13/10.
+//  Copyright __MyCompanyName__ 2010. All rights reserved.
+//
+
+#import "MiniPianoAppDelegate.h"
+#import "MiniPianoViewController.h"
+
+#import <AVFoundation/AVFoundation.h>
+#import "PPStation.h"
+#import "MPPianobarPlayerController.h"
+
+@implementation MiniPianoAppDelegate
+
+@synthesize window;
+@synthesize viewController;
+
+-(void)pianobarDidLogin:(PPPianobarController *)thePianobar{
+	NSLog(@"Login!");
+	
+	if([thePianobar loadStations]){
+		NSArray *stations = [thePianobar stations];
+		for(PPStation *station in stations){
+			NSLog(@"Station is named %@ -   %@",[station name], station);
+			
+			if([[station name] rangeOfString:@"ubstep"].location != NSNotFound){
+				[thePianobar playStationWithID:[station stationID]];
+			}
+		}
+		
+		MPPianobarPlayerController *player = [[MPPianobarPlayerController alloc] init];
+		player.pianobar = thePianobar;
+		
+		[navigationController pushViewController:player animated:YES];
+	}else{
+		NSLog(@"Couldn't load stations");
+	}
+}
+
+-(void)pianobarWillLogin:(PPPianobarController *)pianobar{}
+
+-(void)pianobar:(PPPianobarController *)pianobar didBeginPlayingSong:(PPTrack *)song{
+	
+}
+
+-(void)pianobar:(PPPianobarController *)pianobar didBeginPlayingChannel:(PPStation *)channel{}
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {    
+	[[AVAudioSession sharedInstance] setCategory:AVAudioSessionCategoryPlayback error:nil];
+    
+	navigationController = [[UINavigationController alloc] initWithRootViewController:viewController];
+	
+    // Override point for customization after app launch    
+    [window addSubview:navigationController.view];
+    [window makeKeyAndVisible];
+	
+	NSString *pandoraEmail = nil;
+	NSString *pandoraPassword = nil;
+	
+	if(pandoraEmail && pandoraPassword){
+		pianobar = [[PPPianobarController alloc] initWithUsername:pandoraEmail
+													  andPassword:pandoraPassword];
+		pianobar.delegate = self;
+		[pianobar login];
+	}
+	
+	return YES;
+}
+
+- (void)applicationWillResignActive:(UIApplication *)application {
+    /*
+     Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
+     Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+     */
+}
+
+
+- (void)applicationDidEnterBackground:(UIApplication *)application {
+    /*
+     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
+     If your application supports background execution, called instead of applicationWillTerminate: when the user quits.
+     */
+}
+
+
+- (void)applicationWillEnterForeground:(UIApplication *)application {
+    /*
+     Called as part of  transition from the background to the inactive state: here you can undo many of the changes made on entering the background.
+     */
+}
+
+
+- (void)applicationDidBecomeActive:(UIApplication *)application {
+    /*
+     Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+     */
+}
+
+
+- (void)applicationWillTerminate:(UIApplication *)application {
+    /*
+     Called when the application is about to terminate.
+     See also applicationDidEnterBackground:.
+     */
+}
+
+- (void)dealloc {
+    [viewController release];
+    [window release];
+    [super dealloc];
+}
+
+
+@end
